2d10b7def9668b4997547e544d6b5bbc3642f375
dongdongsw <fishman4535@gmail.com>
2025-12-05 10:20:23 +0900

    1111

---
+++ SpringBootJspProject/src/main/java/com/sist/web/dao/SpringBootJspProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sist.web.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJspProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspProjectApplication.class, args);
+	}
+
+}
